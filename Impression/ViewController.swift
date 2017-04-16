b1a0d5edb8321e3b5390fb33b96b2038394e4336
SeongHo <hsh@dotincorp.com>
2017-04-17 00:38:10 +0900

    Make mockup design

---
+++ Impression/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  Impression
+//
+//  Created by 홍성호 on 2017. 4. 17..
+//  Copyright © 2017년 홍성호. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        self.tableView.backgroundColor = UIColor.clear
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    // MARK: - Table View
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell.init()
+        cell.textLabel?.text = "\(indexPath.row)"
+        
+        return cell
+    }
+    
+    
+
+}
+
